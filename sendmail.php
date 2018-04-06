8258bdbc6d6a656f24d5cb347478063c6dd8776c
Ubuntu <ubuntu@ip-172-31-18-38.us-east-2.compute.internal>
2018-04-06 20:19:56 +0000

    sendmail.php

--- sendmail.php
+++ sendmail.php
@@ -1,17 +1,3 @@
-<?php
-
-// Import the Postmark Client Class:
-require_once('./vendor/autoload.php');
-use Postmark\PostmarkClient;
-
-$client = new PostmarkClient("f4b930b8-1b85-4f1a-a781-ca7b3d7cc844");
-
-// Send an email:
-$sendResult = $client->sendEmail(
-  "sender@example.org",
-  "jleong8@hawaii.edu",
-  "Hello from Postmark!",
-  "This is just a friendly 'hello' from your friends at Postmark."
-);
-
-?>
+<html>
+Hello world
+</html>
