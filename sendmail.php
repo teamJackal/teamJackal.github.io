4892dfe7e0d95b3d1d055b1c5d41fe33481f1f87
Ubuntu <ubuntu@ip-172-31-24-51.us-east-2.compute.internal>
2018-04-07 08:27:49 +0000

    Added email functionality

--- sendmail.php
+++ sendmail.php
@@ -1,3 +1,38 @@
+<?php
+    use PHPMailer\PHPMailer\PHPMailer;
+    use PHPMailer\PHPMailer\Exception;
+
+    require 'PHPMailer/Exception.php';
+    require 'PHPMailer/PHPMailer.php';
+    require 'PHPMailer/SMTP.php';
+
+    $mail = new PHPMailer;
+
+    $mail->isSMTP();
+
+    $mail->setFrom('jleong8@hawaii.edu', 'Jason');
+    $mail->addAddress('herm8888@hawaii.edu', 'Herman');
+
+    $mail->Username = 'AKIAJKMXLA2V5ULPPF5Q';
+    $mail->Password = 'AhIrRhRPbXGahocLaZ9PrRUqEhZPyaudl8ZITBJsRGSr';
+    $mail->Host = 'email-smtp.us-west-2.amazonaws.com';
+
+    $mail->Subject = 'Test Mail';
+    $mail->Body = 'Test mail body words';
+
+    $mail->SMTPAuth = true;
+
+    $mail->SMTPSecure = 'tls';
+    $mail->Port = 587;
+    $mail->isHTML(true);
+
+    if(!$mail->send()) {
+        echo "Email not sent. " , $mail->ErrorInfo , PHP_EOL;
+    } else {
+        echo "Email sent!" , PHP_EOL;
+    }
+?>
+
 <html>
-Hello world
+    Test
 </html>
