1a4dd191339a5d8ffa44f717611501f31876de69
Ubuntu <ubuntu@ip-172-31-24-51.us-east-2.compute.internal>
2018-04-10 00:23:12 +0000

    Modified sendmail
    * Separation of function call and mail sending functions.
    * Mail sending functions contain credentials so needed separation.

--- sendmail.php
+++ sendmail.php
@@ -1,36 +1,8 @@
 <?php
-    use PHPMailer\PHPMailer\PHPMailer;
-    use PHPMailer\PHPMailer\Exception;
+   include "./awssesconnect.php";
 
-    require 'PHPMailer/Exception.php';
-    require 'PHPMailer/PHPMailer.php';
-    require 'PHPMailer/SMTP.php';
+   use PHPMailer\PHPMailer\PHPMailer;                                                                                      use PHPMailer\PHPMailer\Exception;                                                                                                                                                                                                             
 
-    function sendmail($warningType, $message) {
-      $mail = new PHPMailer;
-
-      $mail->isSMTP();
-
-      $mail->setFrom('jleong8@hawaii.edu', 'Warning');
-      $mail->addAddress('herm8888@hawaii.edu', 'Herman');
-
-      $mail->Username = 'AKIAJKMXLA2V5ULPPF5Q';
-      $mail->Password = 'AhIrRhRPbXGahocLaZ9PrRUqEhZPyaudl8ZITBJsRGSr';
-      $mail->Host = 'email-smtp.us-west-2.amazonaws.com';
-
-      $mail->Subject = $warningType;
-      $mail->Body = $message;
-
-      $mail->SMTPAuth = true;
-
-      $mail->SMTPSecure = 'tls';
-      $mail->Port = 587;
-      $mail->isHTML(true);
-
-      if(!$mail->send()) {
-          echo "Email not sent. " , $mail->ErrorInfo , PHP_EOL;
-      } else {
-          echo "Email sent!" , PHP_EOL;
-      }
-    }
+   $mail = new PHPMailer();
+   sendmail($mail, 'Warning Test', 'Test', 'Test Message');
 ?>
