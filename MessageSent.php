7830063ecfa71e1517c04fcc1cdc2308a782c0c3
jleong8 <jleong8@hawaii.edu>
2018-04-26 20:44:36 -1000

    test

--- MessageSent.php
+++ MessageSent.php
@@ -1,8 +1,7 @@
 <?php include('sendmail.php'); ?>
+<?php include('sendsms.php'); ?>
 
-<?php
-  include 'sendsms.php';
-
-  echo sendsms("hello from messageSent", $params);
+<?
+  echo sendsms("hello from messageSenttt", $params);
 
 ?>
