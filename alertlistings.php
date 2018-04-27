69f93ce1cede75cf0b9f44fe5b56f7d04a454961
jleong8 <jleong8@hawaii.edu>
2018-04-26 22:53:50 -1000

    s

--- alertlistings.php
+++ alertlistings.php
@@ -2,7 +2,7 @@
 <?
 $employee_id = $_GET['id'];
 
-$sql = "SELECT * FROM `employee_log` WHERE `employee_id` = '".$employee_id."'";
+$sql = "SELECT * FROM `employee_log`";
 echo $sql;
 $sel = $pdo->prepare($sql);
 $sel->execute();
@@ -18,7 +18,7 @@ $result = $sel->fetchAll();
   <link rel="stylesheet" href="stylesheets/login.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
- 
+
 <body style="background: #95C19B;">
 
 
