81abe9582fe0522338070141a5b3d5714e5f7a20
Ubuntu <ubuntu@ip-172-31-24-51.us-east-2.compute.internal>
2018-04-27 07:09:13 +0000

    Modified sendmail.php
    * Function now takes email as parameter as well.

--- sendmail.php
+++ sendmail.php
@@ -5,5 +5,12 @@
    use PHPMailer\PHPMailer\Exception;
 
    $mail = new PHPMailer();
-   sendmail($mail, 'Warning Test', 'Test', 'Test Message');
+   sendmail($mail, 'Warning Test', 'Test', 'Test Message', $_POST["email"]);
 ?>
+
+<html>
+   <form action="sendmail.php" method="post">
+      E-mail: <input type="text" name="email"><br>
+      <input type="submit">
+   </form>
+</html>
