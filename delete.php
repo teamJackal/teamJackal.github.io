172960f8ba91e13f4099cbff8317af39ad217097
jleong8 <jleong8@hawaii.edu>
2018-04-27 19:32:56 -1000

    s

--- delete.php
+++ delete.php
@@ -20,6 +20,28 @@ if($result) {
 }
 ?>
 
+<!DOCTYPE html>
+<html>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>ALERT SYSTEM</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="stylesheets/login.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+
+<body style="background: #95C19B;">
+
+
+<div id="header" class="container-fluid" style="background-color: #233C15 !important;">
+  <div class="container-fluid text-center">
+    <img class="img-responsive fluid" src="images/Header.png">
+    <div id="header-text">HAWAII EMERGENCY ALERT SYSTEM</div>
+  </div>
+  <button id="back-button" onclick="window.location.href='index.html'" type="button" class="btn btn-default btn-lg">
+    <span class="glyphicon glyphicon-circle-arrow-left" aria-hidden="true"></span> BACK
+  </button>
+</div>
 <div id="body" class="container center">
   <div class="thumbnail text-center">
     <form action="delete.php" method="POST">
@@ -37,3 +59,6 @@ if($result) {
     </form>
   </div>
 </div>
+
+</body>
+</html>
