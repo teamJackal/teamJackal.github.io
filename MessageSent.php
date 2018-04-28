257be5f0dbeb6ec3f65f87a9ffac02a911b56439
jleong8 <jleong8@hawaii.edu>
2018-04-27 21:24:30 -1000

    s

--- MessageSent.php
+++ MessageSent.php
@@ -5,3 +5,13 @@
   include 'sendsms.php';
 
 ?>
+<?
+
+$employee_id = $_GET['id'];
+$sent_value = "1";
+
+$sql = "UPDATE `employee_log` SET `sent` = '".$sent_value."' WHERE `employee_id` = '".$employee_id."' ORDER BY `lastUpdated` ASC LIMIT 1 ";
+$sel = $pdo->prepare($sql);
+$sel->execute();
+
+?>
